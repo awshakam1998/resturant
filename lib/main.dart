072b50e3674b8c32962fcc00b34d1f15ed2a11db
awshakam1998 <awshakam1998@gmail.com>
2022-04-05 16:53:04 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:resturant/model/cart_model.dart';
+import 'package:resturant/pages/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => MyCart(),
+      child: MaterialApp(
+        title: 'Flutter Food Ordering',
+        showSemanticsDebugger: false,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
